7860df70281d22711ecff008699ca921a4f203ad
mpessini <marceloppessini@gmail.com>
2022-08-28 20:04:57 -0300

    Refactor views - fix routes, and quotes

--- api/views.py
+++ api/views.py
@@ -13,8 +13,8 @@ from .models import Post
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
-        'api/token',
-        'api/token/refresh',
+        'token',
+        'token/refresh',
         'signup/',
         'posts/',
         'post/new/'
@@ -36,6 +36,7 @@ def createUser(request):
     data = request.data
     context = {'username': data['username']}
     message = render_to_string('registration-account.txt.j2', context)
+    html_message = render_to_string('registration-account.html.j2', context)
     serializer = UserSerializer(data=data)
     if serializer.is_valid(raise_exception=True):
         try:
@@ -46,10 +47,11 @@ def createUser(request):
                 settings.EMAIL_HOST_USER,
                 [data['email']],
                 fail_silently=False,
+                html_message=html_message
             )
-            return Response(serializer.data)
+            return Response(serializer.data, status=201)
         except:
-            raise serializers.ValidationError({"Username": "Username already exists"})
+            raise serializers.ValidationError({'username': 'username already exists.'})
 
 @api_view(['GET'])
 def getPosts(request):
@@ -62,7 +64,7 @@ def getPosts(request):
 def createPost(request):
     data = request.data
     if (len(data['post_message']) == 0):
-        raise serializers.ValidationError({"Post": "Post can't be empty."})
+        raise serializers.ValidationError({'Post': "Post can't be empty."})
     post_message = Post.objects.create(
         owner = request.user,
         post_message=data['post_message']
